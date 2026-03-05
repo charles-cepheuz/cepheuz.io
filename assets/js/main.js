6d0c394812634bed73cf0467f6c7ec21468fb151
CEPHEUZ <charles@cepheuz.io>
2026-03-05 00:19:31 -0300

    GET

--- assets/js/main.js
+++
@@ -1,5 +0,0 @@
-// Efeito simples de header ao rolar
-window.addEventListener("scroll", function() {
-  const header = document.getElementById("header");
-  header.classList.toggle("scrolled", window.scrollY > 50);
-});
